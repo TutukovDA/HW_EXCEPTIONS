c65364db9a8586fb528785d856b1af0d75e849eb
Dmitriy Tyutyukov <dmitriy.tyutyukov@ifellow.ru>
2024-02-29 23:40:09 +0300

    added maven.yml

--- src/main/java/AlreadyExistsException.java
+++ src/main/java/AlreadyExistsException.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 
 public class AlreadyExistsException extends RuntimeException {
     public AlreadyExistsException(String message) {
+
         super(message);
     }
 }
